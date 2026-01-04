c73b08164e824c0450d364cf179acc2bfcb06e0c
nathalices <nathmatielo@hotmail.com>
2026-01-04 12:28:31 -0300

    ajuste arquivos de infra

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-692614315984-statefile"
-    key = "env:/dev/rds/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "692614315984-terraform-lock"
-  }
-}
--- terraform/data-rds.tf
+++
@@ -1,3 +0,0 @@
-data "aws_db_instance" "mariadb" {
-  db_instance_identifier = var.db_instance_identifier
-}
